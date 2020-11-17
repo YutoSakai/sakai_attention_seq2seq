b1b59cd45d1a67fc76b08cb01e4f49aa9d37a67b
YutoSakai <43488753+YutoSakai@users.noreply.github.com>
2020-11-17 14:41:32 +0900

    _update_vobab実装 datasetをシャッフル

--- ch08/tweet_to_dataset.py
+++ ch08/tweet_to_dataset.py
@@ -54,6 +54,8 @@ def load_data(file_name='~/tweet/tweet2020-11-15嬉しいわ.txt', size=100, see
     split_at = len(x) - len(x) // 10
     (x_train, x_test) = x[:split_at], x[split_at:]
     (t_train, t_test) = t[:split_at], t[split_at:]
+    print(x_train)
+    print(t_train)
     return (x_train, t_train), (t_test, t_test)
 
 
