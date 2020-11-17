86195500f462ecbe4baec8235c887eb0d7a1eb86
YutoSakai <43488753+YutoSakai@users.noreply.github.com>
2020-11-17 14:39:00 +0900

    _update_vobab実装 datasetをシャッフル

--- ch08/tweet_to_dataset.py
+++ ch08/tweet_to_dataset.py
@@ -1,25 +1,64 @@
 import sys
 sys.path.append('..')
+import numpy
+import MeCab
+import os
 
-tweet_path = 'tweet/tweet2020-11-15嬉しいわ.txt'
+
+m = MeCab.Tagger("-Owakati")
+id_to_word = {}
+word_to_id = {}
 
 
-def load_data(tweet_path='tweet/tweet2020-11-15嬉しいわ.txt', size=100):
-    x = []
-    t = []
-    train_x = []
-    train_t = []
-    test_x = []
-    test_t = []
-    with open(tweet_path, 'r') as f:
+def _update_vocab(txt):
+    words = m.parse(txt).split(" ")
+    print(words)
+    for i, word in enumerate(words):
+        if word not in word_to_id:
+            tmp_id = len(word_to_id)
+            word_to_id[word] = tmp_id
+            id_to_word[tmp_id] = word
+    print(id_to_word)
+    print(word_to_id)
+
+def load_data(file_name='tweet/tweet2020-11-15嬉しいわ.txt', size=100, seed=1984):
+    file_path = os.path.dirname(os.path.abspath(__file__)) + '/' + file_name
+
+    if not os.path.exists(file_path):
+        print('No file: %s' % file_name)
+        return None
+
+    x, t = [], []
+    with open(file_path, 'r') as f:
         data = f.readlines()
         for i in range(size):
             if (i % 2) == 0:
                 x.append(data[i][4:])
             else:
                 t.append(data[i][4:])
-    train_x = x[:size-10]
-    train_t = t[:size-10]
-    test_x = x[size-10:]
-    test_t = t[size-10:]
-    return train_x, train_t, test_x, test_t
+
+    # create vocab dict
+    for i in range(len(x)):
+        _update_vocab(x[i])
+        _update_vocab(t[i])
+
+    # shuffle
+    indices = numpy.arange(len(x))
+    if seed is not None:
+        numpy.random.seed(seed)
+    numpy.random.shuffle(indices)
+    x = x[indices]
+    t = t[indices]
+
+    # 10% for validation set
+    split_at = len(x) - len(x) // 10
+    (x_train, x_test) = x[:split_at], x[split_at:]
+    (t_train, t_test) = t[:split_at], t[split_at:]
+    return (x_train, t_train), (t_test, t_test)
+
+
+def get_vocab():
+    return word_to_id, id_to_word
+
+# print(m.parse("わ～～嬉しいなぁありがとう。").split(" "))
+_update_vocab("わ～～嬉しいなぁありがとう。")
