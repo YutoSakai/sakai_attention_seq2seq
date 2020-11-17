a1ff4e0d587be259cb0bd0f9381f3038dc079cc9
YutoSakai <43488753+YutoSakai@users.noreply.github.com>
2020-11-17 16:23:04 +0900

    trainを編集

--- ch08/train.py
+++ ch08/train.py
@@ -15,13 +15,13 @@ from ch07.peeky_seq2seq import PeekySeq2seq
 
 # データの読み込み
 (x_train, t_train), (x_test, t_test) = load_data()
-char_to_id, id_to_char = get_vocab()
+word_to_id, id_to_word = get_vocab()
 
 # 入力文を反転
 x_train, x_test = x_train[:, ::-1], x_test[:, ::-1]
 
 # ハイパーパラメータの設定
-vocab_size = len(char_to_id)
+vocab_size = len(word_to_id)
 wordvec_size = 16
 hidden_size = 256
 batch_size = 128
@@ -45,7 +45,7 @@ for epoch in range(max_epoch):
         question, correct = x_test[[i]], t_test[[i]]
         verbose = i < 10
         correct_num += eval_seq2seq(model, question, correct,
-                                    id_to_char, verbose, is_reverse=True)
+                                    id_to_word, verbose, is_reverse=True)
 
     acc = float(correct_num) / len(x_test)
     acc_list.append(acc)
